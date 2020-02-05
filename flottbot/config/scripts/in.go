428a527f49406fb85a34f0e52bd009f4c58b930c
MrDr-Professor <brolliarj@yahoo.com>
2020-02-05 10:29:46 -0800

    updated in for database storage

--- flottbot/config/scripts/in.go
+++ flottbot/config/scripts/in.go
@@ -1,111 +1,27 @@
 package main
 
 import (
-        "encoding/json"
         "fmt"
-        "io/ioutil"
         "log"
-        "net/http"
-        "os"
         "time"
 
-        "golang.org/x/net/context"
-        "golang.org/x/oauth2"
-        "golang.org/x/oauth2/google"
-        "google.golang.org/api/sheets/v4"
+	"golang.org/x/net/context"
 
         //"go.mongodb.org/mongo-driver/bson"
-        "go.mongodb.org/mongo-driver/mongo"
+        
+	"go.mongodb.org/mongo-driver/mongo"
         "go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Time struct {
-    Uid string
     Timestamp time.Time
     In bool
-    Range string
-}
-
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-        // The file token.json stores the user's access and refresh tokens, and is
-        // created automatically when the authorization flow completes for the first
-        // time.
-        tokFile := "token.json"
-        tok, err := tokenFromFile(tokFile)
-        if err != nil {
-                tok = getTokenFromWeb(config)
-                saveToken(tokFile, tok)
-        }
-        return config.Client(context.Background(), tok)
-}
-
-// Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-        authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-        fmt.Printf("Go to the following link in your browser then type the "+
-                "authorization code: \n%v\n", authURL)
-
-        var authCode string
-        if _, err := fmt.Scan(&authCode); err != nil {
-                log.Fatalf("Unable to read authorization code: %v", err)
-        }
-
-        tok, err := config.Exchange(context.TODO(), authCode)
-        if err != nil {
-                log.Fatalf("Unable to retrieve token from web: %v", err)
-        }
-        return tok
-}
-
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-        f, err := os.Open(file)
-        if err != nil {
-                return nil, err
-        }
-        defer f.Close()
-        tok := &oauth2.Token{}
-        err = json.NewDecoder(f).Decode(tok)
-        return tok, err
-}
-
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-        fmt.Printf("Saving credential file to: %s\n", path)
-        f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-        if err != nil {
-                log.Fatalf("Unable to cache oauth token: %v", err)
-        }
-        defer f.Close()
-        json.NewEncoder(f).Encode(token)
 }
 
 func main() {
-    /////////////////  Google Sheets Stuff  ///////////////////////
-    b, err := ioutil.ReadFile("credentials.json")
-    if err != nil {
-        log.Fatalf("Unable to read client secret file: %v", err)
-    }
-
-    // If modifying these scopes, delete your previously saved token.json.
-    config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
-    if err != nil {
-        log.Fatalf("Unable to parse client secret file to config: %v", err)
-    }
-    client := getClient(config)
-
-    srv, err := sheets.New(client)
-    if err != nil {
-        log.Fatalf("Unable to retrieve Sheets client: %v", err)
-    }
-    /////////////////  End Google Sheets Stuff  ///////////////////////
-
-
-
     /////////////////  MongoDB Stuff  ///////////////////////
     // Set client options
-    clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+    clientOptions := options.Client().ApplyURI("mongodb://database:27017")
 
     // Connect to MongoDB
     mongoclient, err := mongo.Connect(context.TODO(), clientOptions)
@@ -124,28 +40,13 @@ func main() {
     /////////////////  End MongoDB Stuff  ///////////////////////
 
 
-    // Prints the names and majors of students in a sample spreadsheet:
-    // https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
-    spreadsheetId := "1gZUsGqqJPLvFNm5QNLYjc-O0ZvfvCi9jip6PXffgGGI"
-    writeRange := "Sheet1!B1:E"
-
-    var vr sheets.ValueRange
-
-    myval := []interface{}{ time.Now().Format("01/02/2006"), nil, nil, "In Time: " + time.Now().Format("01/02/2006 03:04:05 PM")}
-    vr.Values = append(vr.Values, myval)
-
-    sheet_resp, err := srv.Spreadsheets.Values.Append(spreadsheetId, writeRange, &vr).ValueInputOption("RAW").Do()
-    if err != nil {
-        log.Fatalf("Unable to retrieve data from sheet. %v", err)
-    }
-    fmt.Println("write results: ", sheet_resp.Updates.UpdatedRange)
-
-    time_in := Time{"daniel", time.Now(), true, sheet_resp.Updates.UpdatedRange}
+    time_in := Time{time.Now(), true}
     insertResult, err := collection.InsertOne(context.TODO(), time_in)
     if err != nil {
         log.Fatal(err)
     }
 
     fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+    fmt.Println("Clocked In")
 
 }
